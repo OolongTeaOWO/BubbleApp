731f155dea60684787d697e311edf5cb08f1c89b
OolongTea <gogo34781@gmail.com>
2026-09-19 16:04:29 +0800

    加入專案檔案。

---
+++ BubbleApp/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace BubbleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            checkBox1 = new CheckBox();
+            colorDialog1 = new ColorDialog();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(149, 5);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(86, 19);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 5);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(76, 5);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(52, 50);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(checkBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            MouseClick += Form1_MouseClick;
+            MouseDown += Form1_MouseDown;
+            MouseMove += Form1_MouseMove;
+            MouseUp += Form1_MouseUp;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private ColorDialog colorDialog1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ BubbleApp/Form1.cs
@@ -0,0 +1,162 @@
+using System.Drawing.Drawing2D;
+using System.Timers;
+
+namespace BubbleApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int size = 10;
+        Color set_color = Color.Red;
+        Graphics g;
+        int i = 0;
+        int level = 100;
+        int s = -5;
+
+        void Form_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                size += 5;
+            }
+            else
+            {
+                size -= 5;
+            }
+            label1.Text = "Size= " + size.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MouseWheel += Form_MouseWheel;
+            label1.Text = "Size= " + size.ToString();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            set_color = colorDialog1.Color;
+            pictureBox1.BackColor = set_color;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                set_color = colorRandom();
+                pictureBox1.BackColor = set_color;
+            }
+        }
+
+        Color colorRandom()
+        {
+            Random r = new Random();
+            return Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255));
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            g = this.CreateGraphics();
+
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    {
+                        g.FillEllipse(new SolidBrush(set_color), e.X, e.Y, size, size);
+                        break;
+                    }
+
+                case MouseButtons.Right:
+                    {
+                        Color drawColor = Color.FromArgb(
+                                set_color.R * level / 100,
+                                set_color.G * level / 100,
+                                set_color.B * level / 100
+                            );
+
+                        g.FillEllipse(new SolidBrush(drawColor), e.X, e.Y, size, size);
+
+                        pictureBox1.BackColor = drawColor;
+
+                        level += s;
+
+                        if (level <= 0)
+                        {
+                            level = 0;
+                            s = 5;
+                        }
+                        else if (level >= 100)
+                        {
+                            level = 100;
+                            s = -5;
+                        }
+
+                        break;
+                    }
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            g = this.CreateGraphics();
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    {
+                        int holdtimeCounter = 0;
+                        timer1.Interval = 2000;
+                        timer1.Tick -= timer1_Tick;
+
+                        g.FillEllipse(new SolidBrush(set_color), e.X, e.Y, size, size);
+                        break;
+                    }
+
+                case MouseButtons.Right:
+                    {
+                        Color drawColor = Color.FromArgb(
+                                set_color.R * level / 100,
+                                set_color.G * level / 100,
+                                set_color.B * level / 100
+                            );
+
+                        g.FillEllipse(new SolidBrush(drawColor), e.X, e.Y, size, size);
+
+                        pictureBox1.BackColor = drawColor;
+
+                        level += s;
+
+                        if (level <= 0)
+                        {
+                            level = 0;
+                            s = 5;
+                        }
+                        else if (level >= 100)
+                        {
+                            level = 100;
+                            s = -5;
+                        }
+
+                        break;
+                    }
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer1.Tick -= timer1_Tick;
+
+
+        }
+    }
+}
