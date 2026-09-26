767253183c4560a5aa85043e47605a2092c81850
OolongTea <gogo34781@gmail.com>
2026-09-26 20:15:41 +0800

    edit

--- BubbleApp/Form1.cs
+++ BubbleApp/Form1.cs
@@ -31,6 +31,7 @@ namespace BubbleApp
         }
         //app
         //a
+        //app
         private void Form1_Load(object sender, EventArgs e)
         {
             this.MouseWheel += Form_MouseWheel;
